347e690939b466009cc999dd0401bbd4341bfa24
desion <wdxin1322@sohu.com>
2017-05-30 19:57:07 +0800

    add dashboard for management

---
+++ web/conf.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+terminal_servers_conf = [
+	{
+		"host": "52.10.82.82",
+		"port": 8089,
+		"tag": "word2vec",
+	},
+	{
+		"host": "52.10.82.82",
+		"port": 8089,
+		"tag": "user profile",
+	}
+]
