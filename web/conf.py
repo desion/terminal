655753a083d2ff38adccb8e50b1cad15957176a6
desion <wdxin1322@sohu.com>
2017-05-30 19:58:16 +0800

    update conf

--- web/conf.py
+++ web/conf.py
@@ -1,12 +1,12 @@
 #coding:utf-8
 terminal_servers_conf = [
 	{
-		"host": "52.10.82.82",
+		"host": "127.0.0.1",
 		"port": 8089,
 		"tag": "word2vec",
 	},
 	{
-		"host": "52.10.82.82",
+		"host": "127.0.0.1",
 		"port": 8089,
 		"tag": "user profile",
 	}
